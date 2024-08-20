6d24f4e5dbbfb0b2552a0c0945985da6186390e7
thieu.duong <duongthieu1995@gmail.com>
2024-08-20 20:50:52 +0700

    update account_service

---
+++ Account_Service/src/main/java/com/thieuduong/account_service/controller/AccountController.java
@@ -0,0 +1,41 @@
+package com.thieuduong.account_service.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thieuduong.account_service.model.AccountDTO;
+import com.thieuduong.account_service.model.MessageDTO;
+import com.thieuduong.account_service.model.StatisticDTO;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+	@Autowired
+	KafkaTemplate<String, Object> kafkaTemplate;
+
+	@PostMapping("/new")
+	public AccountDTO create(@RequestBody AccountDTO account) {
+		StatisticDTO stat = new StatisticDTO(
+			"Account " + account.getEmail() + " is created", new Date());
+		stat.setStatus(false);
+
+		// send notificaiton
+		MessageDTO messageDTO = new MessageDTO();
+		messageDTO.setTo(account.getEmail());
+		messageDTO.setToName(account.getName());
+		messageDTO.setSubject("Welcome to JMaster.io");
+		messageDTO.setContent("JMaster is online learning platform.");
+		messageDTO.setStatus(false);
+
+		kafkaTemplate.send("notification", messageDTO);
+		kafkaTemplate.send("statistic", stat);
+
+		return account;
+	}
+}
---
+++ Account_Service/src/main/java/com/thieuduong/account_service/model/MessageDTO.java
@@ -0,0 +1,60 @@
+package com.thieuduong.account_service.model;
+
+public class MessageDTO {
+	
+	private int id;
+	private String to;
+	private String toName;
+	private String subject;
+	private String content;
+
+	private boolean status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getToName() {
+		return toName;
+	}
+
+	public void setToName(String toName) {
+		this.toName = toName;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
